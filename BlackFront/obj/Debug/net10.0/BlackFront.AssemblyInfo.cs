d62c70186ffb84d2bd9576ca6fee38baa6e1412e
mrSashaman_ <psasha2811@gmail.com>
2026-09-06 13:56:17 +1200

    v1.6: Add next article navigation

--- BlackFront/obj/Debug/net10.0/BlackFront.AssemblyInfo.cs
+++ BlackFront/obj/Debug/net10.0/BlackFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d8c530181ada69d687b910a826393dfb60443a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a070848a6aacf6ace735fc0c3415005b6dcdcc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
