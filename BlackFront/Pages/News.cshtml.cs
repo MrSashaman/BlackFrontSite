13c0231a560581fc6e6765b9236a5f5d4ab3b9bc
mrSashaman_ <psasha2811@gmail.com>
2026-09-06 14:08:37 +1200

    v1.7: Add publication search

--- BlackFront/Pages/News.cshtml.cs
+++ BlackFront/Pages/News.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,24 +15,47 @@ public class NewsModel : PageModel
 
     public List<Post> LatestPosts { get; set; } = new();
 
+
+    [BindProperty(SupportsGet = true)]
+    public string? Search { get; set; }
+
+
     public async Task OnGetAsync()
     {
-        FeaturedPosts = await _context.Posts
+        var query = _context.Posts
             .Include(p => p.Category)
             .Include(p => p.PostBadges)
                 .ThenInclude(pb => pb.Badge)
-            .Where(p => p.IsFeatured)
             .AsNoTracking()
+            .AsQueryable();
+
+
+        if (!string.IsNullOrWhiteSpace(Search))
+        {
+            var search = Search.Trim();
+
+            query = query.Where(p =>
+                EF.Functions.Like(
+                    p.Title,
+                    $"%{search}%"
+                )
+                ||
+                EF.Functions.Like(
+                    p.Content,
+                    $"%{search}%"
+                )
+            );
+        }
+
+
+        FeaturedPosts = await query
+            .Where(p => p.IsFeatured)
             .OrderByDescending(p => p.FeaturedAt)
             .ToListAsync();
 
 
-        LatestPosts = await _context.Posts
-            .Include(p => p.Category)
-            .Include(p => p.PostBadges)
-                .ThenInclude(pb => pb.Badge)
+        LatestPosts = await query
             .Where(p => !p.IsFeatured)
-            .AsNoTracking()
             .OrderByDescending(p => p.CreatedAt)
             .ToListAsync();
     }
--- BlackFront/obj/Debug/net10.0/BlackFront.AssemblyInfo.cs
+++ BlackFront/obj/Debug/net10.0/BlackFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a070848a6aacf6ace735fc0c3415005b6dcdcc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62c70186ffb84d2bd9576ca6fee38baa6e1412e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
